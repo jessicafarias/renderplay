f3f3c26ab72ee854ba58034f386de175a4b7e592
Grandheilm <elyixso@gmail.com>
2026-02-26 19:21:58 -0600

    [Refactor] Removed absolute positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <h1
-        className="absolute top-8 left-1/2 -translate-x-1/2 z-10 whitespace-nowrap m-0 text-5xl font-bold tracking-[4px] text-[#00ff2a] font-courier text-shadow-glow-title"
-      >
+    <main className="flex flex-col items-center">
+      <h1 className="z-10 whitespace-nowrap m-0 text-5xl font-bold tracking-[4px] text-[#00ff2a] font-courier text-shadow-glow-title py-8">
         RenderPlay Feature Canvas Preview
       </h1>
       <ModelViewer />
