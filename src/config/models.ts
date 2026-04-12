10ea90abe0b870676eea1ef71ad3f5cfc2fd248a
jessicafarias <jessica.farias.rosado@gmail.com>
2026-04-11 22:26:30 -0600

    [Feature] scale 1.1

--- src/config/models.ts
+++ src/config/models.ts
@@ -6,7 +6,7 @@ export interface ModelConfig {
 
 export const MODELS: ModelConfig[] = [
   // { name: "Buster Drone", path: "/models/choza/ChozaM_E8C93442.gltf", scale: 1 },
-  { name: "Choza", path: "/models/chozav2/Allinone.gltf", scale: 1 },
+  { name: "Choza", path: "/models/chozav2/Allinone.gltf", scale: 1.1 },
   // { name: "Helmet",       path: "/models/helmet/scene1.gltf",  scale: 0.02 },
   // { name: "Casa",         path: "/models/casa/CASA.gltf",     scale: 0.3},
 ];
